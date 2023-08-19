00f6007467702f67b55fc6b10b89604f370480c4
Example User <eUser@example.com>
2023-08-19 07:12:29 +0000

    add fact_attendance table

--- modelling/export.sql
+++ modelling/export.sql
@@ -1,3 +1,4 @@
 COPY fact_compclass TO 'export/fact_compclass.csv' (HEADER, DELIMITER ',');
 COPY fact_compstudent TO 'export/fact_compstudent.csv' (HEADER, DELIMITER ',');
 COPY dim_school TO 'export/dim_school.csv' (HEADER, DELIMITER ',');
+COPY fact_attendance TO 'export/fact_attendance.csv' (HEADER, DELIMITER ',');
